577d09b40deede7dc4537d40d45fd55fb5042f38
Kristof Buts <github@kristofbuts.be>
2019-02-14 14:05:02 +0100

    Added btn functionality

--- app/src/main/java/be/kristofbuts/android/customeroverview/MainActivity.kt
+++ app/src/main/java/be/kristofbuts/android/customeroverview/MainActivity.kt
@@ -2,13 +2,17 @@ package be.kristofbuts.android.customeroverview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import be.kristofbuts.android.customeroverview.model.Customer
 import be.kristofbuts.android.customeroverview.model.getCustomers
 import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var btnPrev: Button
+    private lateinit var btnNext: Button
+    private lateinit var imgCustomer: ImageView
     private lateinit var txtID: TextView
     private lateinit var txtName: TextView
     private lateinit var txtCompany: TextView
@@ -17,15 +21,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var txtRegistration: TextView
     private lateinit var txtActive: TextView
 
+    private lateinit var customers: Array<Customer>
+    private var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.customers = getCustomers()
 
         this.initialiseViews()
         this.addEventHandlers()
     }
 
     private fun initialiseViews() {
+        this.btnPrev = findViewById(R.id.btnPrev)
+        this.btnNext = findViewById(R.id.btnNext)
+
+        this.imgCustomer = findViewById(R.id.imgCustomer)
         this.txtID = findViewById(R.id.txtId)
         this.txtName = findViewById(R.id.txtName)
         this.txtCompany = findViewById(R.id.txtCompany)
@@ -34,14 +47,24 @@ class MainActivity : AppCompatActivity() {
         this.txtRegistration = findViewById(R.id.txtRegistration)
         this.txtActive = findViewById(R.id.txtActive)
 
-        this.fillCustomerInfo(getCustomers()[0])
+        this.fillCustomerInfo(this.customers[counter])
     }
 
     private fun addEventHandlers() {
         // add btn events
+        this.btnPrev.setOnClickListener {
+            this.decreaseCounter()
+            this.fillCustomerInfo(this.customers[counter])
+        }
+
+        this.btnNext.setOnClickListener {
+            this.increaseCounter()
+            this.fillCustomerInfo(this.customers[counter])
+        }
     }
 
     private fun fillCustomerInfo(c: Customer) {
+        this.imgCustomer.setImageDrawable(getDrawable(c.image))
         this.txtID.text = c.id.toString()
         this.txtName.text = String.format("%s %s", c.firstName, c.lastName)
         this.txtCompany.text = c.company
@@ -49,5 +72,13 @@ class MainActivity : AppCompatActivity() {
         this.txtCalls.text = c.callsToSerivceLine.toString()
         this.txtRegistration.text = SimpleDateFormat("yyyy-MM-dd").format(c.registrationDate)
         this.txtActive.text = if (c.isActive) getString(R.string.yes) else getString(R.string.no)
+    }
+
+    private fun increaseCounter() {
+        this.counter = (this.counter + 1) % this.customers.size
+    }
+
+    private fun decreaseCounter() {
+        this.counter = (this.counter + this.customers.size - 1) % this.customers.size
     }
 }
--- app/src/main/java/be/kristofbuts/android/customeroverview/model/DataFunctions.kt
+++ app/src/main/java/be/kristofbuts/android/customeroverview/model/DataFunctions.kt
@@ -4,7 +4,8 @@ import be.kristofbuts.android.customeroverview.R
 import java.util.*
 
 fun getCustomers(): Array<Customer> {
-    val c1 = Customer(0, "Michael", "Lucas", "Geekus", "michael.lucas@geekus.biz", 9, Date(115,5,15), true, R.drawable.michael)
+    val c1 = Customer(0, "Michael", "Lucas", "Geekus", "michael.lucas@geekus.biz", 9, Date(115,4,15), true, R.drawable.michael)
+    val c2 = Customer(1, "Briggs", "Puckett", "Bicol", "briggs.puckett@bicol.co.uk", 18, Date(115,11,9), true, R.drawable.briggs)
 
-    return arrayOf(c1)
+    return arrayOf(c1, c2)
 }
